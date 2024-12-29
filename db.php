47c1e02393b3bda4bc8f945e08b298750e934986
ALXDROID <81201701+ALXDROID@users.noreply.github.com>
2024-12-29 02:55:39 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
-$DB_HOST = getenv("MYSQLHOST");
-$DB_USER = getenv("MYSQLUSER");
-$DB_PASSWORD = getenv("MYSQLPASSWORD");
-$DB_NAME = getenv("MYSQLDATABASE");
-$DB_PORT = getenv("MYSQLPORT");
 
-// Crear conexión
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+$DBURL = "mysql://root:kUfcvmpoRcVdxKgpoioLkbTIxmEizFwt@autorack.proxy.rlwy.net:23890/railway"
+$db = parse_url($DBURL);    
+$host = $db['host'];
+$user = $db['user'];
+$password = $db['pass'];
+$dbname = ltrim($db['path'], '/');
+$port = $db['port'];
+
+// Establecer la conexión
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     echo json_encode(["error" => "Connection failed: " . $conn->connect_error]);
