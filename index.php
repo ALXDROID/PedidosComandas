3da629cdd886262baf1aa4f8068731b74205b4b2
ALXDROID <81201701+ALXDROID@users.noreply.github.com>
2024-12-29 09:24:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
    
-    <title>AJAX MySQL Example</title>
+    <title>AJAX MySQL VBA</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
