26b89b93eaaaada42c48e763214f4171ee062414
ALXDROID <81201701+ALXDROID@users.noreply.github.com>
2024-12-29 03:49:09 -0300

    Update getOrders.php

--- getOrders.php
+++ getOrders.php
@@ -1,5 +1,7 @@
 
 <?php
+
+header('Content-Type: application/json');
 $DBURL = "mysql://root:kUfcvmpoRcVdxKgpoioLkbTIxmEizFwt@autorack.proxy.rlwy.net:23890/railway"
 $db = parse_url($DBURL);    
 $host = $db['host'];
